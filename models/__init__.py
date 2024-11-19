d0136a92a5c2dd9414596b79bf6cfae96b81a33c
Poetrilin <31932@81992@qq.com>
2024-11-19 13:21:34 +0800

    fix gitignore

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Description: This file is used to import all the models in the models directory.
+from .PINN import PINN
+from .FLS import FLS
+from .PINNsformer import PINNsformer
+from .KAN import KAN
+
+__all__ = ['PINN','FLS','PINNsformer','KAN']
